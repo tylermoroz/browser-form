7ed142ec2e3284742c4edaaf68b9e1dc00ab2328
tylermoroz <moroztyler@gmail.com>
2023-10-31 14:48:57 -0600

    Add validity constraints to each input using javascript and set :invalid and :valid pseudo-classes.

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const zip = document.getElementById("zip-code");
 const password = document.getElementById("password");
 const confirmPass = document.getElementById("confirm-password");
+const form = document.getElementById("form");
 
-// const passwordConfirmation = () => {
-//     if(confirmPassword.value == password.value){
-//         passwordConfirmation.setCustomValidity("");
-//     } else {
-//         passwordConfirmation.setCustomValidity("Must match password");
-//     }
-// }
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("Please enter a valid email address");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+country.addEventListener("input", (event) => {
+  if (country.validity.patternMismatch) {
+    country.setCustomValidity("Countries cannot contain numbers or symbols");
+  } else {
+    country.setCustomValidity("");
+  }
+});
+
+zip.addEventListener("input", (event) => {
+  if (zip.validity.patternMismatch) {
+    zip.setCustomValidity("Please use a proper ZIP Code format");
+  } else {
+    zip.setCustomValidity("");
+  }
+});
 
 confirmPass.addEventListener("input", (event) => {
   if (confirmPass.value == password.value) {
     confirmPass.setCustomValidity("");
   } else {
     confirmPass.setCustomValidity("Must match password");
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (form.checkValidity()) {
+    prompt("2 thumbs up!");
   }
 });
